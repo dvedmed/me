676f90c4fa6bc52c573f30d6cc2428e2ea11f78a
dvedmed <dvedmedenko@ecare21.com>
2021-06-20 20:09:19 +0300

    fix: db credentials, listening port remove: corn options

--- server.js
+++ server.js
@@ -51,7 +51,7 @@ app.get("/", (req, res) => {
 require('./app/routes/user.routes')(app);*/
 
 // set port, listen for requests
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3031;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
