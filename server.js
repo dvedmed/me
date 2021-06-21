1f61098c954719699477991891fb5ff15834af25
dvedmed <dvedmedenko@ecare21.com>
2021-06-21 23:49:51 +0300

    port to 8080

--- server.js
+++ server.js
@@ -30,7 +30,7 @@ require('./app/routes/auth.routes')(app);
 require('./app/routes/user.routes')(app);
 
 // set port, listen for requests
-const PORT = process.env.PORT || 80;
+const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}.`);
 });
